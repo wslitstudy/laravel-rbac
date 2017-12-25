cfb8983eaf491f2c0244d418f427c2e3d2637dd6
wangshenglin <850692811@qq.com>
2017-12-25 12:29:10 +0800

    update dir

--- symfony/config/bundles.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    App\AdminBundle\AdminBundle::class => ['all' => true],
-    App\HomeBundle\HomeBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-];
--- symfony/src/Resources/views/login.html.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: wsl
- * Date: 17-12-19
- * Time: 下午3:46
- */
